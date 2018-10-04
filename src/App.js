ec093dfd0a2d61f4182ff510313247bae6fd7b99
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-04 19:33:50 -0400

    large photo page complete

--- src/App.js
+++ src/App.js
@@ -83,6 +83,36 @@ class App extends Component {
             </div>
           </div>
         </section> */}
+        <section className="section">
+          <div className="container">
+            <nav className="breadcrumb" aria-label="breadcrumbs">
+              <ul>
+                <li>
+                  <a href="#">
+                    <span className="icon is-small">
+                      <i className="fas fa-home" aria-hidden="true" />
+                    </span>
+                    <span>Home</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span>{Data.pandas.title}</span>
+                  </a>
+                </li>
+                <li className="is-active">
+                  <a href="#">
+                    <span>{Data.pandas.photos[0].title}</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <h3 className="title">{Data.pandas.photos[0].title}</h3>
+            <figure className="image">
+              <img src={Data.pandas.photos[0].imageURL} />
+            </figure>
+          </div>
+        </section>
       </div>
     )
   }
