8314d4828c0b7900939ca59bdaf50bb5b6367af2
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-04 18:08:47 -0400

    header and nav complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Data from './Data.json'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <section class="hero">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">
+                <a>Things I Like</a>
+              </h1>
+              <h2 class="subtitle">A Photo Gallery by Benjamin Kent Jehl</h2>
+            </div>
+          </div>
+        </section>
+        <section class="section">
+          <div class="container">
+            <nav class="breadcrumb" aria-label="breadcrumbs">
+              <ul>
+                <li className="is-active">
+                  <a href="#">
+                    <span class="icon is-small">
+                      <i class="fas fa-home" aria-hidden="true" />
+                    </span>
+                    <span>Home</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <div class="columns is-mobile">
+              <div class="column">1</div>
+              <div class="column">2</div>
+            </div>
+          </div>
+        </section>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
