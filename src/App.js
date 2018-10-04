9fdf79701ffb67e3dcc2fdf5d345de6042c0c330
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-04 19:11:29 -0400

    panda photo list nav complete

--- src/App.js
+++ src/App.js
@@ -19,7 +19,72 @@ class App extends Component {
             </div>
           </div>
         </section>
-        <CategoryList />
+        {/* <CategoryList /> */}
+        <section className="section">
+          <div className="container">
+            <nav className="breadcrumb" aria-label="breadcrumbs">
+              <ul>
+                <li>
+                  <a href="#">
+                    <span className="icon is-small">
+                      <i className="fas fa-home" aria-hidden="true" />
+                    </span>
+                    <span>Home</span>
+                  </a>
+                </li>
+                <li className="is-active">
+                  <a href="#">
+                    <span>{Data.pandas.title}</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <div className="columns is-mobile">
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <div className="content">
+                      <h2>
+                        <a>{Data.pandas.title}</a>
+                      </h2>
+                    </div>
+
+                    <div className="content">{Data.pandas.description}</div>
+                  </div>
+                  <div className="card-image">
+                    <figure className="image">
+                      <img
+                        src={Data.pandas.photos[0].imageURL}
+                        alt="Panda Bear"
+                      />
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <div className="content">
+                      <h2>
+                        <a>{Data.miniatures.title}</a>
+                      </h2>
+                    </div>
+
+                    <div className="content">{Data.miniatures.description}</div>
+                  </div>
+                  <div className="card-image">
+                    <figure className="image">
+                      <img
+                        src={Data.miniatures.photos[0].imageURL}
+                        alt="Panda Bear"
+                      />
+                    </figure>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     )
   }
